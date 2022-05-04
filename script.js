f78c67a106869abc6a54ee9a9c837c4f7f4eac14
Ella M <74894801+thatmexicanforeigner@users.noreply.github.com>
2022-05-04 15:52:01 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var list = document.querySelectorAll("li");
+var list = document.getElementsByTagName("li");
+
+var removeButton = document.createElement("button");
+removeButton.classList.add("deleteButton");
+
+// if (finished) {
+//     removeButton.appendChild(document.createTextNode("remove"));
+//     removeButton.classList = "deleteButton";
+//     li.appendChild(removeButton);
+
+//     removeButton.addEventListener("click", function() {
+//         this.parentElement.remove();
+//     });
+// } else {
+//     this.getElementsByClassName("deleteButton")[0].remove();
+// }
+
+function toggleClassDoneOnAndOff(event) {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("done");
+    }
+}
+ul.addEventListener("click", toggleClassDoneOnAndOff);
+
 
 function inputLength(){
     return input.value.length;
@@ -23,6 +48,11 @@ function addListAfterKeypress(event) {
         if (inputLength() > 0 && event.keyCode === 13) {
             createListElement();
         }
+}
+
+function removeElement(li) {
+    var element = document.getElementById(li);
+    element.parentNode.removeChild(li);
 }
 
 button.addEventListener("click", addListAfterClick)
